99374dee9799755dcd7cc3cb50b29ac55895235e
justin <justin@pop-os.localdomain>
2022-10-03 21:14:39 -0500

    Committer: justin <justin@pop-os.localdomain>
    too much stuff

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
     plugins: [
       require('postcss-import'),
       require('tailwindcss'),
+      require('@tailwindcss/typography'),
       require('autoprefixer'),
     ]
   }
